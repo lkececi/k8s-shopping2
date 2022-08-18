26cad140bdbbb44b342ba4d1dca5b2c8239840b1
Levent Kececi <levent.kececi@gmail.com>
2022-08-18 19:11:51 +0300

    API consuming

--- Solution1/Shopping.Client/Controllers/HomeController.cs
+++ Solution1/Shopping.Client/Controllers/HomeController.cs
@@ -1,22 +1,29 @@
 ﻿using System.Diagnostics;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
-using Shopping.Client.Data;
+using Newtonsoft.Json;
 using Shopping.Client.Models;
 
 namespace Shopping.Client.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient _httpClient;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IHttpClientFactory httpClientFactory, ILogger<HomeController> logger)
         {
             _logger = logger;
+            _httpClient = httpClientFactory.CreateClient("ShoppingAPIClient");
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(ProductContext.Products);
+            var response = await _httpClient.GetAsync("/product");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList = JsonConvert.DeserializeObject<IEnumerable<Product>>(content);
+
+            return View(productList);
         }
 
         public IActionResult Privacy()
